26e0b82ad6f9040f985fd4cf40cb5542709ef90b
the-suraj <surajsjjha@gmail.com>
2019-11-16 18:23:38 +0530

    added encryption method

---
+++ gaali/encrypt.js
@@ -0,0 +1,49 @@
+let str = "+ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+console.log(str);
+let caesarCipher = (str, key = 0) => {
+    let encryptedText = "";
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        // A -> 65 & a -> 97
+        // Z -> 90 & z -> 122
+        let charCode = char.charCodeAt(0);
+        let newCharCode;
+        if (65 <= charCode && charCode <= 90) {
+            // encrypting capital letter
+            if (65 <= charCode+key && charCode+key <= 90) {
+                newCharCode = charCode + key;
+            }
+            else if (90 < charCode+key) {
+                newCharCode = 65 + (charCode + key - 91)%26;
+            }
+            else if (charCode+key < 65) {
+                newCharCode = 90 - (64 - charCode - key)%26;
+            }
+        }
+        else if (97 <= charCode && charCode <= 122) {
+            // encrypting small letter
+            if (97 <= charCode+key && charCode+key <= 122) {
+                newCharCode = charCode + key;
+            }
+            else if (122 < charCode+key) {
+                newCharCode = 97 + (charCode + key - 123)%26;
+            }
+            else if (charCode+key < 97) {
+                newCharCode = 122 - (96 - charCode - key)%26;
+            }
+        } else {
+            // keep other symbols same
+            newCharCode = charCode;
+        }
+        encryptedText += String.fromCharCode(newCharCode);
+        console.log(`charcode: ${charCode} => ${char}, newCharCode: ${newCharCode} => ${String.fromCharCode(newCharCode)}`);
+    };
+    return encryptedText;
+};
+let encrypt = (str) => caesarCipher(str, 1);
+let decrypt = (str) => caesarCipher(str, -1);
+// console.log(encrypt(str));
+console.log(decrypt(str));
+
+
