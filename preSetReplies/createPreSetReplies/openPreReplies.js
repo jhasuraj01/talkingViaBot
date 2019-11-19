d4f8f28647699b769ea7cde0f4a9cdb9228b760a
the-suraj <surajsjjha@gmail.com>
2019-11-20 02:31:27 +0530

    feature to read an uploaded file is added

---
+++ preSetReplies/createPreSetReplies/openPreReplies.js
@@ -0,0 +1,29 @@
+let callback_open_file_btn_clicked = (eve) => {
+    file_in.click();
+}
+
+let WriteMessagesOnScreen = (data) => {
+    console.log(data);
+}
+
+let open_file_fn = () => {
+    if (file_in.value) {
+        let file = file_in.files[0];
+
+        // a reader is set to read the uploaded file.
+        let reader = new FileReader();
+        reader.onload = (eve) => {
+            let data = eve.target.result;
+            WriteMessagesOnScreen(JSON.parse(data));
+        }
+        reader.readAsText(file_in.files[0], "UTF-8");
+    }
+    else {
+        console.error('no file is selected');
+    }
+}
+let open_file_btn = document.getElementById('open-file-btn');
+open_file_btn.addEventListener('click', callback_open_file_btn_clicked);
+
+let file_in = document.getElementById('file-in');
+file_in.addEventListener('input', open_file_fn);
